944d3bef91d608570a00204b99693afdb908c9b6
Danilo Lima <danilomoura@outlook.com>
2016-02-25 23:44:07 -0300

    Eliminei os links e ficou bem mais simples. O programa já cria o arquivo, permite ao user inserir pacientes, permite retirar o primeiro da fila, e permite resetar arquivo. Falta imprimir o arquivo.

--- ED2_01/ED2_01/Program.cs
+++ ED2_01/ED2_01/Program.cs
@@ -5,69 +5,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
-       Rascunho da estrutura do Arquivo:
-       #  TIPO_SANGUINEO   PRIMEIRA_POSIÇÃO    ULTIMO_INSERIDO
-       1. "Tipo A",null,null          
-       2. "Tipo B",7,10
-       3. "Tipo AB",6,11
-       4. "Tipo O",null,null       
-       # NOME_PACIENTE    TIPO_SANGUINEO  PROXIMA_POSIÇÃO
-       6. "Fulano de Tal",AB,8
-       7. "Cicrano",B,9
-       8. "Ronaldo",AB,11
-       9. "Rivaldo",B,10
-       10. "Gaucho",B,null
-       11. "Roberto",AB,null      
-*/
-
 namespace ED2_01
 {
     class ListaPacientes
     {
         public string entrada = null;
+        //o arquivo vai estar na pasta meus documentos
         public static string caminhoArquivo = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//ListaPacientes.txt";
-        public static String[] cabecalho = { "Tipo A,null,null",
-                                    "Tipo B,null,null",
-                                    "Tipo AB,null,null",
-                                    "Tipo O,null,null" };
+        public static String[] cabecalho = { " " };
 
         public class Paciente
         {
             public String nome;
             public String tipoSanguineo;
-            public int proximo;
         }
 
        
         static void Main(string[] args)
         {
-            inicializar();
-            ListaPacientes lista = new ListaPacientes();
-            Paciente paciente = new Paciente();
+            string entrada;
             while (true)
             {
-                Console.WriteLine("Insira o nome do(a) próximo(a) paciente");
-                lista.entrada = Console.ReadLine();
-                paciente.nome = lista.entrada;
+                Console.WriteLine("");
+                Console.WriteLine("==================== MENU PRINCIPAL =========================");
+                Console.WriteLine("Comandos: 'limpar arquivo', 'cadastrar', 'convocar', 'sair'");
+                entrada = Console.ReadLine();
 
-                Console.WriteLine("Insira o tipo sanguineo dele(a)");
-                lista.entrada = Console.ReadLine();
-                paciente.tipoSanguineo = lista.entrada;
-
-                string[] linha = { paciente.nome + "," + paciente.tipoSanguineo };
-                File.AppendAllLines(caminhoArquivo, linha);
-
-
+                if (entrada == "limpar arquivo") inicializar();
+                if (entrada == "cadastrar") cadastrarPaciente();
+                if (entrada == "convocar") convocarPaciente();
+                if (entrada == "sair") break;
             }
-            
 
 
         }
 
+        private static void cadastrarPaciente()
+        {
+            ListaPacientes lista = new ListaPacientes();
+            Paciente paciente = new Paciente();
+            List<string> listaDeLinhas = new List<string>();
+
+            while (true)
+            {
+                
+                Console.WriteLine("Digite 'voltar' para voltar ao menu principal");
+                Console.WriteLine("Ou insira o nome do(a) próximo(a) paciente");
+                lista.entrada = Console.ReadLine();
+                paciente.nome = lista.entrada;
+                if (lista.entrada == "voltar") break;
+
+                Console.WriteLine("Insira o tipo sanguineo dele(a)");
+                lista.entrada = Console.ReadLine();
+                paciente.tipoSanguineo = lista.entrada;
+                if (lista.entrada == "voltar") break;
+
+                listaDeLinhas.Add(paciente.nome + "            " + paciente.tipoSanguineo);
+            }
+
+            string[] arrayDeLinhas = listaDeLinhas.ToArray();
+            File.AppendAllLines(caminhoArquivo, arrayDeLinhas);
+            Console.WriteLine("Pacientes cadastrados com sucesso! Voltando ao menu...");
+        }
+
+        private static void convocarPaciente() {
+            string[] arrayArquivo = File.ReadAllLines(caminhoArquivo);
+            List<string> listaArquivo = arrayArquivo.ToList();
+            listaArquivo.RemoveAt(1);
+            arrayArquivo = listaArquivo.ToArray();
+            File.WriteAllLines(caminhoArquivo, arrayArquivo);
+            Console.WriteLine("Paciente removido com sucesso! Voltando ao menu...");
+        }
+
+
         private static void inicializar()
         {
             File.WriteAllLines(caminhoArquivo, cabecalho);
+            Console.WriteLine("Arquivo inicializado com sucesso! Abrindo menu...");
+        }
+
+        public static void imprimir()
+        {
+            //fazer
         }
     }
 }
