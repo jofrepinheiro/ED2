f8ee715ff5da54e60563f0eda5f1a437bd1796dd
jofrepinheiro <jofrepinheiro@gmail.com>
2016-03-01 00:31:27 -0300

    Criação da Classe Paciente
    Adicionados mais campos

---
+++ ED2_01/ED2_01/Paciente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED2_01
+{
+    class Paciente
+    {
+        private String nome;
+        private String matricula;
+        private String tipoSanguineo;
+        private String orgao;
+        private char genero;
+
+
+        public String Nome{
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public String Matricula {
+            get {
+                return matricula;
+            }
+            set {
+                matricula = value;
+            }
+        }
+
+        public String TipoSanguineo
+        {
+            get
+            {
+                return tipoSanguineo;
+            }
+            set
+            {
+                tipoSanguineo = value;
+            }
+        }
+
+        public char Genero
+        {
+            get
+            {
+                return genero;
+            }
+            set
+            {
+                genero = value;
+            }
+        }
+
+        public String Orgao
+        {
+            get
+            {
+                return orgao;
+            }
+            set
+            {
+                orgao = value;
+            }
+        }
+
+    }
+}
